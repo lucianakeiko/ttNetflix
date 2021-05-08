f7c0b694ae82a60b995b494fd0572cc7cc7102cc
Keik@ <lucianakeiko@gmail.com>
2021-05-08 12:39:25 -0300

    base de dados importada

---
+++ data-raw/netflix.R
@@ -0,0 +1,8 @@
+#Carregar pacote
+devtools::load_all(".")
+
+#Salvar dados brutos
+netflix <- atualizar_dados("data-raw/netflix.csv")
+
+#Colocar no pacote
+usethis::use_data(netflix, overwrite = TRUE)
